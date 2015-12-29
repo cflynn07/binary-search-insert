592d7dcdd794100d8f155f53e00cd40a4fcb4f40
Casey Flynn <cflynn.us@gmail.com>
2015-12-28 23:11:43 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+
+module.exports = binarySearchInsert
+
+/**
+ * Value falls between two values
+ * 4
+ * [0, 3, 5, 7, 9]
+ * [0, 3, 4, 5, 7, 9]
+ *
+ * Value is equal to a value
+ * 5
+ * [0, 3, 5, 7, 9]
+ * [0, 3, 4, 5, 5, 7, 9]
+ *
+ * Value is equal to a value
+ * 5
+ * [0, 3, 5, 5, 7, 9]
+ * [0, 3, 4, 5, 5, 7, 9]
+ */
+
+/**
+ *
+ */
+function binarySearchInsert (sortedArray, item, comparator) {
+  var low = 0;
+  var high = sortedArray.length - 1;
+  var lastIndex = high;
+
+  if (high === -1) {
+    sortedArray.push(item);
+    return sortedArray;
+  }
+
+  while (low <= high) {
+    // https://github.com/darkskyapp/binary-search
+    // http://googleresearch.blogspot.com/2006/06/extra-extra-read-all-about-it-nearly.html
+    let mid = low + (high - low >> 1);
+    let cmp = comparator(sortedArray[mid], item);
+    if (cmp < 0.0) {
+      // searching too low
+      if (mid + 1 > lastIndex) {
+        // place item at end of array
+        sortedArray.push(item);
+        return sortedArray;
+      }
+
+      let cmp2 = comparator(sortedArray[mid + 1], item);
+      if (cmp2 >= 0.0) {
+        // right item is too high or equal, insert in item to right of mid
+        sortedArray.splice(mid + 1, 0, item);
+        return sortedArray;
+      }
+
+      low = mid + 1;
+    } else if (cmp > 0.0) {
+      // searching too high
+      if (mid === 0) {
+        // place item at beginning of array
+        sortedArray.splice(0, 0, item);
+        return sortedArray;
+      }
+
+      let cmp2 = comparator(sortedArray[mid - 1], item);
+      if (cmp2 <= 0.0) {
+        // left item is too low or equal, insert in item to left of mid
+        sortedArray.splice(mid - 1, 0, item);
+        return sortedArray;
+      }
+
+      high = mid - 1;
+    } else {
+      // mid equals item
+      sortedArray.splice(mid, 0, item);
+      return sortedArray;
+    }
+  }
+}
---
+++ src/index_test.js
@@ -0,0 +1,108 @@
+/**
+ *
+ */
+
+var Code = require('Code');
+var Lab = require('lab');
+
+var binarySearchInsert = require('./index');
+
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var expect = Code.expect;
+var it = lab.it;
+
+describe('lib/index', function () {
+  var comparator = function (a, b) { return a - b; };
+
+  it('should insert item if sortedArray is empty', function (done) {
+    var sortedArray = [];
+    var item = 5;
+    binarySearchInsert(sortedArray, item, comparator);
+    expect(sortedArray.length).to.equal(1);
+    expect(sortedArray[0]).to.equal(5);
+    done();
+  });
+
+  it('should insert item if sortedArray has length 1 and its item is higher', function (done) {
+    var sortedArray = [10];
+    var item = 5;
+    binarySearchInsert(sortedArray, item, comparator);
+    expect(sortedArray.length).to.equal(2);
+    expect(sortedArray[0]).to.equal(5);
+    expect(sortedArray[1]).to.equal(10);
+    done();
+  });
+
+  it('should insert item if sortedArray has length 1 and its item is lower', function (done) {
+    var sortedArray = [1];
+    var item = 5;
+    binarySearchInsert(sortedArray, item, comparator);
+    expect(sortedArray.length).to.equal(2);
+    expect(sortedArray[0]).to.equal(1);
+    expect(sortedArray[1]).to.equal(5);
+    done();
+  });
+
+  it('should place item in sortedArray between lower and higher values', function (done) {
+    var sortedArray = [1, 3, 5, 7];
+    var sortedArrayTarget = [1, 3, 4, 5, 7];
+    var item = 4;
+    binarySearchInsert(sortedArray, item, comparator);
+    expect(sortedArray.length).to.equal(5);
+    expect(sortedArray).to.deep.equal(sortedArrayTarget);
+    done();
+  });
+
+  it('should place item in sortedArray which contains a single matching value', function (done) {
+    var sortedArray = [1, 3, 5, 7];
+    var sortedArrayTarget = [1, 3, 5, 5, 7];
+    var item = 5;
+    binarySearchInsert(sortedArray, item, comparator);
+    expect(sortedArray.length).to.equal(5);
+    expect(sortedArray).to.deep.equal(sortedArrayTarget);
+    done();
+  });
+
+  it('should place item in sortedArray which contains a multiple matching values', function (done) {
+    var sortedArray = [1, 3, 5, 5, 5, 7];
+    var sortedArrayTarget = [1, 3, 5, 5, 5, 5, 7];
+    var item = 5;
+    binarySearchInsert(sortedArray, item, comparator);
+    expect(sortedArray.length).to.equal(7);
+    expect(sortedArray).to.deep.equal(sortedArrayTarget);
+    done();
+  });
+
+  it('should handle sortedArray of length 2^18', function (done) {
+    var len = Math.pow(2, 20);
+    var sortedArraySequentialValues = [];
+    var sortedArrayDuplicateValues = [];
+    for (let i = 0; i < len; i++) {
+      sortedArraySequentialValues.push(i);
+    }
+
+    for (let i = 0; i < (len / 4) | 0; i++) {
+      sortedArrayDuplicateValues.push(i);
+      sortedArrayDuplicateValues.push(i);
+      sortedArrayDuplicateValues.push(i);
+      sortedArrayDuplicateValues.push(i);
+    }
+
+    var item = 500000;
+    binarySearchInsert(sortedArraySequentialValues, item, comparator);
+    expect(sortedArraySequentialValues.length).to.equal(len + 1);
+    expect(sortedArraySequentialValues.splice(499998, 6)).to.deep.equal([
+      499998, 499999, 500000, 500000, 500001, 500002
+    ]);
+
+    item = 43750;
+    binarySearchInsert(sortedArrayDuplicateValues, item, comparator);
+    expect(sortedArrayDuplicateValues.length).to.equal(len + 1);
+    expect(sortedArrayDuplicateValues.splice(174998, 10)).to.deep.equal([
+      43749, 43749, 43750, 43750, 43750, 43750, 43750, 43751, 43751, 43751
+    ]);
+    done();
+  });
+});
